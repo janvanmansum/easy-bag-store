bcfdbfe68c761a8057909c4459eb67e949087674
Jan van Mansum <jan.van.mansum@dans.knaw.nl>
2018-01-02 17:55:05 +0100

    WIP

--- src/main/scala/nl.knaw.dans.easy.bagstore/ArchiveStream.scala
+++ src/main/scala/nl.knaw.dans.easy.bagstore/ArchiveStream.scala
@@ -18,11 +18,13 @@ package nl.knaw.dans.easy.bagstore
 import java.io.OutputStream
 import java.nio.file.{ Files, Path }
 
+import nl.knaw.dans.lib.logging.DebugEnhancedLogging
+import org.apache.commons.compress.archivers.tar.TarArchiveOutputStream
 import org.apache.commons.compress.archivers.{ ArchiveOutputStream, ArchiveStreamFactory }
 import resource.ManagedResource
 
 import scala.language.implicitConversions
-import scala.util.Try
+import scala.util.{ Success, Try }
 
 object ArchiveStreamType extends Enumeration {
   type ArchiveStreamType = Value
@@ -34,8 +36,8 @@ import nl.knaw.dans.easy.bagstore.ArchiveStreamType._
 /**
  * Specification for an entry in the archive file.
  *
- * @param sourcePath  optional path to an existing file or directory, if None a directory entry will be created
- * @param entryPath the path of the entry in the archive file
+ * @param sourcePath optional path to an existing file or directory, if None a directory entry will be created
+ * @param entryPath  the path of the entry in the archive file
  */
 case class EntrySpec(sourcePath: Option[Path], entryPath: String)
 
@@ -44,7 +46,7 @@ case class EntrySpec(sourcePath: Option[Path], entryPath: String)
  *
  * @param files the files in the TAR ball
  */
-class ArchiveStream(streamType: ArchiveStreamType, files: Seq[EntrySpec]) {
+class ArchiveStream(streamType: ArchiveStreamType, files: Seq[EntrySpec]) extends DebugEnhancedLogging {
 
   implicit private def toArchiveStreamFactory(streamType: ArchiveStreamType.Value): String = {
     streamType match {
@@ -61,19 +63,37 @@ class ArchiveStream(streamType: ArchiveStreamType, files: Seq[EntrySpec]) {
    */
   def writeTo(outputStream: => OutputStream): Try[Unit] = {
     createArchiveOutputStream(outputStream).map(_.acquireAndGet { aos =>
-      files.foreach(addFileToArchiveOutputStream(aos))
-      aos.finish()
+      setLongFileNameSupport(aos)
+      files.toStream.map(addFileToArchiveOutputStream(aos))
+        .map(r => { debug(s"Result is: $r"); r })
+        .find(_.isFailure).getOrElse(finishArchiveOutputStream(aos))
     })
   }
+
+  private def finishArchiveOutputStream(aos: ArchiveOutputStream): Try[Unit] = Try {
+    aos.finish()
+  }
+
+  private def setLongFileNameSupport(aos: ArchiveOutputStream): Unit = {
+    aos match {
+      case tar: TarArchiveOutputStream => tar.setLongFileMode(TarArchiveOutputStream.LONGFILE_TRUNCATE)
+    }
+  }
+
 
   private def createArchiveOutputStream(output: => OutputStream): Try[ManagedResource[ArchiveOutputStream]] = Try {
     resource.managed(new ArchiveStreamFactory("UTF-8").createArchiveOutputStream(streamType, output))
   }
 
   private def addFileToArchiveOutputStream(os: ArchiveOutputStream)(entrySpec: EntrySpec): Try[Unit] = Try {
+    debug(s"Adding entry: ${ entrySpec.entryPath }...")
     val entry = os.createArchiveEntry(entrySpec.sourcePath.map(_.toFile).orNull, entrySpec.entryPath)
+    debug(s"Putting entry: ${ entrySpec.entryPath }...")
     os.putArchiveEntry(entry)
+    debug(s"Copying file data: ${ entrySpec.entryPath }...")
     entrySpec.sourcePath.foreach { case file if Files.isRegularFile(file) => Files.copy(file, os) }
+    debug(s"Closing entry: ${ entrySpec.entryPath }...")
     os.closeArchiveEntry()
+    debug(s"Done adding entry: ${ entrySpec.entryPath }")
   }
 }
